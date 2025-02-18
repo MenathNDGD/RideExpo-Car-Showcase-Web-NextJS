382faadd3333ee2580fdd0f38bfe2b1cc25a9612
MenathNDGD <nadungmenath1@gmail.com>
2025-02-18 16:17:29 +0530

    Add Hero component for showcasing featured content and call-to-action

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Explore. Dream. Drive. Discover Your Next Ride with RideExpo.
+        </h1>
+        <p className="hero__subtitle">
+          Unveil the Beauty of Every Ride – Discover, Compare, and Choose Your
+          Perfect Car.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src={"/hero.png"}
+            alt="RideExpo"
+            fill
+            className="object-contain"
+          />
+
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
