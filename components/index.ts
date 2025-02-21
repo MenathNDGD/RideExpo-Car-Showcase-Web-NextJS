21d646f6270c27a251482015f8a5998bcb135ee9
MenathNDGD <nadungmenath1@gmail.com>
2025-02-21 20:52:01 +0530

    Add SearchBar, CustomFilter, and SearchManufacturer to components export

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "@/components/Hero";
 import CustomButton from "@/components/CustomButton";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import SearchManufacturer from "@/components/SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
