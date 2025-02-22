faade652b2840b2fd959094a4f927e7b394f048a
MenathNDGD <nadungmenath1@gmail.com>
2025-02-22 21:41:10 +0530

    Add CarDetails component to CarCard for enhanced car information display

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 
 import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
+
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -75,6 +77,12 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
