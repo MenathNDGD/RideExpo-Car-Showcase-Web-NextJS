66d978d5c198847f66ad204079852badb23c9712
MenathNDGD <nadungmenath1@gmail.com>
2025-02-24 18:58:22 +0530

    Add ShowMore component to Home page for pagination support

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { fetchCars } from "@/utils";
 import { SearchParams } from "@/types";
 
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
 
 export default async function Home({
@@ -44,6 +44,11 @@ export default async function Home({
                 <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
